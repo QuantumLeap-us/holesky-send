637b944669b3038c03a9ce6983998800a3341eb4
Wawan Darmawan <41707274+wanshade@users.noreply.github.com>
2024-01-12 13:33:39 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+const dotenv = require('dotenv');
+const result = dotenv.config();
+
+if (result.error) {
+  throw result.error;
+}
+
+const infuraApiKey = process.env.INFURA_API_KEY;
+
 const sendForm = document.getElementById('send-form');
 const sendButton = document.getElementById('send-button');
 const outputDiv = document.getElementById('output');
@@ -20,7 +29,7 @@ sendButton.addEventListener('click', async () => {
     try {
       const transaction = await sendTransaction(privateKey, toAddress);
       numTransactions++;
-      outputDiv.innerHTML += `Transaction #${numTransactions} sent from ${transaction.from} with hash: <a href="https://sepolia.etherscan.io/tx/${transaction.transactionHash}" rel="noopener">${transaction.transactionHash}</a><br>`;
+      outputDiv.innerHTML += `Transaction #${numTransactions} sent from ${transaction.from} with hash: <a href="https://holesky.etherscan.io/tx/${transaction.transactionHash}" rel="noopener">${transaction.transactionHash}</a><br>`;
       outputDiv.innerHTML += `Sent ${transaction.value} ETH to ${transaction.to}<br><br>`;
 
     } catch (error) {
@@ -36,7 +45,7 @@ sendButton.addEventListener('click', async () => {
 
 
 async function sendTransaction(privateKey, toAddress) {
-  const web3 = new Web3(new Web3.providers.HttpProvider('https://eth-holesky.blastapi.io/a5a43e8d-7adc-4994-baab-809705e8ebd5'));
+  const web3 = new Web3(new Web3.providers.HttpProvider(`https://eth-holesky.blastapi.io/${infuraApiKey}`));
   const account = web3.eth.accounts.privateKeyToAccount(privateKey);
   const fromAddress = account.address;
 
