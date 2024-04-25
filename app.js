8cdb841d0152ad77d6d204f38e2a14efd1bb3cfc
QuantumLeap <40482494+QuantumLeap-us@users.noreply.github.com>
2024-04-25 22:16:50 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ sendButton.addEventListener('click', async () => {
     .map(key => key.trim())
     .filter(key => key !== '');
 
-const toAddresses = sendForm.elements['to-addresses'].value.split('\n')
+  const toAddresses = sendForm.elements['to-addresses'].value.split('\n')
     .map(address => address.trim())
     .filter(address => address !== '');
-  
+
   if (privateKeys.length === 0) {
     outputDiv.textContent = 'Please enter at least one private key';
     return;
@@ -26,14 +26,15 @@ const toAddresses = sendForm.elements['to-addresses'].value.split('\n')
 
   for (const privateKey of privateKeys) {
     try {
-      const transaction = await sendTransaction(privateKey, toAddresses);
-      numTransactions++;
-      outputDiv.innerHTML += `Transaction #${numTransactions} sent from ${transaction.from} with hash: <a href="https://holesky.etherscan.io/tx/${transaction.transactionHash}" rel="noopener">${transaction.transactionHash}</a><br>`;
-      outputDiv.innerHTML += `Sent ${transaction.value} ETH to ${transaction.to}<br><br>`;
-
+      const transactions = await sendTransactionsBatch(privateKey, toAddresses);
+      numTransactions += transactions.length;
+      transactions.forEach(({ transactionHash, from, to, value }, index) => {
+        outputDiv.innerHTML += `Transaction #${numTransactions - transactions.length + index + 1} sent from ${from} with hash: <a href="https://holesky.etherscan.io/tx/${transactionHash}" rel="noopener">${transactionHash}</a><br>`;
+        outputDiv.innerHTML += `Sent ${value} ETH to ${to}<br><br>`;
+      });
     } catch (error) {
       numErrors++;
-      outputDiv.textContent += `Error sending transaction from ${error.from} to ${error.to}: ${error.message}\n`;
+      outputDiv.textContent += `Error sending transactions from ${error.from}: ${error.message}\n`;
     }
   }
 
@@ -42,46 +43,52 @@ const toAddresses = sendForm.elements['to-addresses'].value.split('\n')
   }
 });
 
-async function sendTransaction(privateKey, toAddresses) {
+async function sendTransactionsBatch(privateKey, toAddresses) {
   const web3 = new Web3(new Web3.providers.HttpProvider('https://eth-holesky.blastapi.io/a5a43e8d-7adc-4994-baab-809705e8ebd5'));
   const account = web3.eth.accounts.privateKeyToAccount(privateKey);
   const balance = await web3.eth.getBalance(account.address);
 
-  // 获取当前的gas价格
   const currentGasPrice = await web3.eth.getGasPrice();
-
-  // 设置gasPrice为当前gas价格加上一个小的优先费用
-  const priorityFee = web3.utils.toWei('1.5', 'gwei'); // 设置优先费用为1.5 Gwei
+  const priorityFee = web3.utils.toWei('1.5', 'gwei');
   const gasPrice = web3.utils.toBN(currentGasPrice).add(web3.utils.toBN(priorityFee));
+
+  const transactions = [];
+  const batchTransactions = [];
 
   for (const toAddress of toAddresses) {
     const transaction = {
       from: account.address,
       to: toAddress,
-      value: web3.utils.toHex(web3.utils.toWei('0.5', 'ether')), // Send 0.5 ETH
-      gas: web3.utils.toHex(21000), // 设置gas限制
-      gasPrice: gasPrice // 使用当前gas价格加上优先费用作为gasPrice
+      value: web3.utils.toHex(web3.utils.toWei('0.5', 'ether')),
+      gas: web3.utils.toHex(21000),
+      gasPrice: gasPrice
     };
 
-    try {
-      const gasEstimate = await web3.eth.estimateGas(transaction);
-      transaction.gas = gasEstimate; // 使用估算的gas值
-
-      // 检查账户余额是否足够支付gas费用
-      const gasLimit = web3.utils.toBN(transaction.gas);
-      const totalGasCost = gasLimit.mul(gasPrice);
-      const accountBalance = web3.utils.toBN(balance);
-
-      if (totalGasCost.gt(accountBalance)) {
-        throw new Error('Insufficient balance to pay gas');
-      }
-
-      const signedTx = await account.signTransaction(transaction);
-      const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
-      console.log('Transaction successful:', receipt.transactionHash);
-    } catch (error) {
-      console.error('Error sending transaction:', error);
-      throw error;
-    }
+    batchTransactions.push(transaction);
   }
+
+  try {
+    const gasEstimates = await Promise.all(batchTransactions.map(tx => web3.eth.estimateGas(tx)));
+    batchTransactions.forEach((tx, index) => {
+      tx.gas = gasEstimates[index];
+    });
+
+    const signedTransactions = await Promise.all(batchTransactions.map(tx => account.signTransaction(tx)));
+    const results = await Promise.all(signedTransactions.map(tx => web3.eth.sendSignedTransaction(tx.rawTransaction)));
+
+    results.forEach((result, index) => {
+      const transaction = batchTransactions[index];
+      transactions.push({
+        transactionHash: result.transactionHash,
+        from: account.address,
+        to: transaction.to,
+        value: web3.utils.fromWei(transaction.value, 'ether')
+      });
+    });
+  } catch (error) {
+    console.error('Error sending transactions:', error);
+    throw { from: account.address, message: error.message };
+  }
+
+  return transactions;
 }
