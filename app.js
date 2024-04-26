15509c1b4b9565a3e76f20304746d31417881ec5
QuantumLeap <40482494+QuantumLeap-us@users.noreply.github.com>
2024-04-26 08:30:59 +0800

    Update app.js

--- app.js
+++ app.js
@@ -44,22 +44,19 @@ sendButton.addEventListener('click', async () => {
 });
 
 async function sendTransactionsBatch(privateKey, toAddresses) {
-  const web3 = new Web3(new Web3.providers.HttpProvider('https://eth-holesky.blastapi.io/a5a43e8d-7adc-4994-baab-809705e8ebd5'));
+  const web3 = new Web3(new Web3.providers.HttpProvider('https://ethereum-holesky.blockpi.network/v1/rpc/ce9f71735a4b346a47f062a8b55fdb4c355a13d1'));
   const account = web3.eth.accounts.privateKeyToAccount(privateKey);
   const balance = await web3.eth.getBalance(account.address);
 
-  // 获取账户的未决交易
-  const pendingTransactions = await web3.eth.getTransactionCount(account.address, 'pending');
-
-  // 获取当前的gas价格
   const currentGasPrice = await web3.eth.getGasPrice();
-
-  // 设置gasPrice为当前gas价格加上一个小的优先费用
-  const priorityFee = web3.utils.toWei('1.5', 'gwei'); // 设置优先费用为1.5 Gwei
+  const priorityFee = web3.utils.toWei('1.5', 'gwei');
   const gasPrice = web3.utils.toBN(currentGasPrice).add(web3.utils.toBN(priorityFee));
 
-  let transactions = []; // 将 const 改为 let
+  const transactions = [];
   const batchTransactions = [];
+
+  // 获取账户的当前 nonce 值
+  let nonce = await web3.eth.getTransactionCount(account.address, 'pending');
 
   for (const toAddress of toAddresses) {
     const transaction = {
@@ -68,11 +65,11 @@ async function sendTransactionsBatch(privateKey, toAddresses) {
       value: web3.utils.toHex(web3.utils.toWei('0.5', 'ether')),
       gas: web3.utils.toHex(21000),
       gasPrice: gasPrice,
-      nonce: pendingTransactions // 使用未决交易的数量作为nonce
+      nonce: web3.utils.toHex(nonce) // 使用当前 nonce 值
     };
 
     batchTransactions.push(transaction);
-    pendingTransactions++; // 增加nonce值,以防止后续交易也被视为替换交易
+    nonce++; // 增加 nonce 值,以防止后续交易也被视为替换交易
   }
 
   try {
@@ -95,8 +92,10 @@ async function sendTransactionsBatch(privateKey, toAddresses) {
     });
   } catch (error) {
     console.error('Error sending transactions:', error);
-    throw { from: undefined, message: error.message };
+    throw { from: account.address, message: error.message };
   }
 
+  return transactions;
+}
   return transactions;
 }
