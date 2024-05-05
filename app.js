e9f61f89b93c21c91468bd7d52c5fc4370904fb0
QuantumLeap <40482494+QuantumLeap-us@users.noreply.github.com>
2024-05-05 21:31:56 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,12 @@ const outputDiv = document.getElementById('output');
 
 sendButton.addEventListener('click', async () => {
   const privateKeys = document.getElementById('private-key').value.split('\n')
-    。map(key => key.trim())
-    。filter(key => key !== '');
+  .map(key => key.trim())
+  .filter(key => key !== '');
 
-  const toAddresses = sendForm.elements['to-addresses'].value.split('\n')
-    。map(address => address.trim())
-    。filter(address => address !== '');
-
+const toAddresses = sendForm.elements['to-addresses'].value.split('\n')
+  .map(address => address.trim())
+  .filter(address => address !== '');
   if (privateKeys.length === 0) {
     outputDiv.textContent = 'Please enter at least one private key';
     return;
